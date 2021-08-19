8426d366d086c5c3175d5cd918f7f82537872bf2
Diogonsc <diogon21@yahoo.com.br>
2021-08-18 21:40:39 -0300

    first commit

---
+++ src/components/NomeCompleto.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Rotulo nome="Nome" >
+					<input type="text" :value="value.nome" @input="alterarNome">
+		</Rotulo>
+		<Rotulo nome="SobreNome">
+					<input type="text" :value="value.sobreNome" @input="alterarSobreNome">
+		</Rotulo>
+  </div>
+</template>
+
+<script>
+import Rotulo from "./Rotulo.vue";
+export default {
+  props:{
+    value: {
+      type: Object,
+      required: true,
+    }
+  },
+  components: {Rotulo},
+  methods: {
+    alterarNome(event) {
+      this.$emit('input', {nome: event.target.value, sobreNome: this.value.sobreNome})
+    },
+    alterarSobreNome(event) {
+      this.$emit('input', {nome: this.value.nome, sobreNome: event.target.value})
+    }
+  }
+};
+</script>
+
+<style>
+</style>
